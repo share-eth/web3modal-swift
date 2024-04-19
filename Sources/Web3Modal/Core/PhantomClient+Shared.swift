22beede4825d28774fd65a15d60cd2cbf4c141f6
mooshee <daniel.hallman@gmail.com>
2024-04-18 23:26:02 -0400

    add phantom singleton

---
+++ Sources/Web3Modal/Core/PhantomClient+Shared.swift
@@ -0,0 +1,13 @@
+//
+//  PhantomClient+Shared.swift
+//
+//
+//  Created by Daniel Hallman on 4/18/24.
+//
+
+import Foundation
+import phantom_swift
+
+extension PhantomClient {
+    public static var shared: PhantomClient?
+}
